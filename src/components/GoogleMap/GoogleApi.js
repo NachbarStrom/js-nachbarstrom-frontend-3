ae32e5d3fa67893701713cc89dd6e2d916f5a1ac
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2018-10-30 19:26:44 +0100

    BUGFIX: Google Maps no longer crashes navigating backwards

--- src/components/GoogleMap/GoogleApi.js
+++ src/components/GoogleMap/GoogleApi.js
@@ -1,43 +1,24 @@
-export const GoogleApi = function(opts) {
+export const GoogleApi = opts => {
   opts = opts || {};
 
-  const apiKey = opts.apiKey;
-  const libraries = opts.libraries || [];
-  const client = opts.client;
   const URL = 'https://maps.googleapis.com/maps/api/js';
-
   const googleVersion = '3';
-  let script = null;
-  let google = (window.google = null);
-  let loading = false;
-  let channel = null;
-  let language = null;
-  let region = null;
 
-  let onLoadEvents = [];
-
-  const url = () => {
-    let url = URL;
-    let params = {
-      key: apiKey,
-      callback: 'CALLBACK_NAME',
-      libraries: libraries.join(','),
-      client: client,
-      v: googleVersion,
-      channel: channel,
-      language: language,
-      region: region
-    };
-
-    let paramStr = Object.keys(params)
-      .filter(k => !!params[k])
-      .map(k => `${k}=${params[k]}`)
-      .join('&');
-
-    return `${url}?${paramStr}`;
+  const requestParams = {
+    key: opts.apiKey,
+    callback: 'CALLBACK_NAME',
+    libraries: (opts.libraries || []).join(','),
+    client: opts.client,
+    v: googleVersion,
+    channel: null,
+    language: null,
+    region: null,
   };
 
-  return url();
-};
+  const paramsPostfix = Object.keys(requestParams)
+    .filter(k => !!requestParams[k])
+    .map(k => `${k}=${requestParams[k]}`)
+    .join('&');
 
-export default GoogleApi;
+  return `${URL}?${paramsPostfix}`;
+};
--- src/components/GoogleMap/GoogleApiComponent.js
+++ src/components/GoogleMap/GoogleApiComponent.js
@@ -1,18 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import cache from './ScriptCache';
-import GoogleApi from './GoogleApi';
+import { Cache } from './Cache';
+import { GoogleApi } from './GoogleApi';
 
-const defaultMapConfig = {};
 export const wrapper = options => WrappedComponent => {
-  const apiKey = options.apiKey;
-  const libraries = options.libraries || ['places', 'drawing'];
-
   class Wrapper extends React.Component {
     constructor(props, context) {
       super(props, context);
-
       this.state = {
         loaded: false,
         map: null,
@@ -21,49 +16,36 @@ export const wrapper = options => WrappedComponent => {
     }
 
     componentDidMount() {
-      const refs = this.refs;
       this.scriptCache.google.onLoad((err, tag) => {
         const maps = window.google.maps;
-        const props = Object.assign({}, this.props, {
-          loaded: this.state.loaded
-        });
-
-        const mapRef = refs.map;
-
-        const node = ReactDOM.findDOMNode(mapRef);
-        let center = new maps.LatLng(this.props.lat, this.props.lng);
-
-        let mapConfig = Object.assign({}, defaultMapConfig, {
-          center,
-          zoom: this.props.zoom
-        });
-
-        this.map = new maps.Map(node, mapConfig);
-
+        const node = ReactDOM.findDOMNode(this.refs.map);
+        const center = new maps.LatLng(this.props.lat, this.props.lng);
+        const mapConfig = { center, zoom: this.props.zoom };
         this.setState({
           loaded: true,
-          map: this.map,
-          google: window.google
+          map: new maps.Map(node, mapConfig),
+          google: window.google,
         });
       });
     }
 
     componentWillMount() {
-      this.scriptCache = cache({
+      this.scriptCache = Cache({
         google: GoogleApi({
-          apiKey: apiKey,
-          libraries: libraries
+          apiKey: options.apiKey,
+          libraries: options.libraries || ['places', 'drawing'],
         })
       });
     }
 
     render() {
-      const props = Object.assign({}, this.props, {
+      const props = {
+        ...this.props,
         loaded: this.state.loaded,
         map: this.state.map,
         google: this.state.google,
-        mapComponent: this.refs.map
-      });
+        mapComponent: this.refs.map,
+      };
 
       return (
         <div style={{ height: '100%' }}>
