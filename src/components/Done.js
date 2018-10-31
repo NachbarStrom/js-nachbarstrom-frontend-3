1f3213f26a0ba388d8cafd6800228b1b0e181fbd
Penny Hung <peininghung@gmail.com>
2018-10-31 17:40:18 +0100

    Change text in "Done" box

--- src/components/Done.js
+++ src/components/Done.js
@@ -15,22 +15,22 @@ export const Done = () => {
 
   const processTimeline = (
     <div className="Rectangle-20">
-      <div className="continue">And so it goes from now:</div>
-      <div className="up-to">up to 1 day</div>
-      <div className="two-weeks">2 weeks</div>
-      <div className="by-arrangement">by arrangement</div>
+      <div className="continue">Was als nächstes passiert:</div>
+      <div className="up-to">Bis 1 Tag</div>
+      <div className="two-weeks">2 Wochen</div>
+      <div className="by-arrangement">auf Termin</div>
       <img src={tickIcon} className="calculate" />
       <div className="Line-7" />
       <img src={callIcon} className="advice" />
       <img src={expertIcon} className="on-site" />
       <img src={truckIcon} className="installation" />
       <div className="Line-6" />
-      <div className="next-steps-title-blue">Calculate</div>
-      <div className="next-steps-blue">recommended price & individual financial plan</div>
-      <div className="next-steps-title-advice">Advice</div>
-      <div className="next-steps-advice">by telephone for detailed system layout </div>
-      <div className="next-steps-title-on-site">On-site</div>
-      <div className="next-steps-on-site">expertise appointment</div>
+      <div className="next-steps-title-blue">Analyse</div>
+      <div className="next-steps-blue">Flächen & Kosten</div>
+      <div className="next-steps-title-advice">Beratung </div>
+      <div className="next-steps-advice">Persönlich & Kompetent</div>
+      <div className="next-steps-title-on-site">Vor Ort Termin</div>
+      <div className="next-steps-on-site">Detailplanung</div>
       <div className="next-steps-title-installation">Installation</div>
       <div className="next-steps-installation">of the new solar system</div>
     </div>
