158d63fb60675e9518a8dc87631d971d8a69bc33
Tomas Ruiz <tomas.ruiz.te@gmail.com>
2018-11-13 21:30:54 +0100

    Update redeploy script to work with deps files

--- redeploy.sh
+++ redeploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git pull
-yarn  # install dependencies
+yarn install --production
 yarn build
 sudo killall node
 nohup sudo $(which serve) -s build --listen 80 >>logs 2>>logs &
